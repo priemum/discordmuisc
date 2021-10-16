7d6f2f10136cb7bfd768d9a8218d8a2611d00dd6
priemum <50713962+priemum@users.noreply.github.com>
2021-10-16 18:31:13 +0200

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -2,7 +2,7 @@ const { MessageEmbed } = require("discord.js");
 const YouTubeAPI = require("simple-youtube-api");
 const { YOUTUBE_API_KEY, LOCALE } = require("../util/EvobotUtil");
 const youtube = new YouTubeAPI(YOUTUBE_API_KEY);
-const i18n = require("i18n");
+const i18n = require("i18n");  
 
 i18n.setLocale(LOCALE);
 
